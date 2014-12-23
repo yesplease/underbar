c258f5f5b659d6205635f78995e3aa82d201134c
Bradley Heinz <bradley.heinz@gmail.com>
2014-12-23 12:21:05 -0600

    Finished map

--- src/underbar.js
+++ src/underbar.js
@@ -124,8 +124,20 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
-  };
 
+    var results = [];
+    if(Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++) {
+        results.push(iterator(collection[i], i, collection));
+      } 
+    } else {
+        for (var prop in collection){
+          results.push(iterator(collection[prop], prop, collection));
+        }
+      }
+    return results;   
+    };
+    
   /*
    * TIP: map is really handy when you want to transform an array of
    * values into a new array of values. _.pluck() is solved for you
