b6001582c9b2756caa326c6eb3ac8b437de69cc4
Bradley Heinz <bradley.heinz@gmail.com>
2014-12-22 23:54:35 -0600

    Finished through 'filter'

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -43,7 +44,7 @@
         return array[array.length-1];
       } else {
         return array.slice(-n);
-      };
+      }
     };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -52,6 +53,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+        }
+      } else {
+        for (var prop in collection) {
+          iterator(collection[prop], prop, collection);
+        }
+      }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -73,7 +83,24 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(value, i, collecion){
+      if (test(value) === true) {
+       result.push(collection[i]);  
+    }
+  });
+    return result;
   };
+
+
+
+
+  //      if (test === true) {
+  //      result.push(collection[i]);  
+  //   }
+  // });
+  //   return result;
+  // };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
